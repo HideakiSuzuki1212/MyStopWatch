46f1ee9bfe124b8d29f0da7be0f84fc4ab19de0a
HideakiSuzuki1212 <choro.syumei@gmail.com>
2020-02-07 00:17:11 +0900

    プレイリストの配列と、for文を追加。

--- js/music.js
+++ js/music.js
@@ -1,4 +1,18 @@
 let playMusic;
+let fileList = [
+  {},
+  {},
+  {},
+  {},
+  {},
+];
+let playList = '<ul>';
+for(let i=0; i < fileList.length; i++){
+    playList +='<li onclick = start_music("'+fileList[i].url+'")>'; //ここの意味がわからん
+    playList += fileList[i].name +'</li>';//ここもよくわからん。
+}
+playList +='</ul>';
+document.getElementById("playListArea").innerHTML = playList;//ここは分かりそう。
 
 function PlayMusic(){
   playMusic = new Audio();
