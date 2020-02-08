25a1acee53d76cdafc0733a3f6133d61e4b26a0f
HideakiSuzuki1212 <choro.syumei@gmail.com>
2020-02-08 15:08:25 +0900

    別の音楽リストを選択したときに、前の音楽を停止する機能を実装。

--- js/music.js
+++ js/music.js
@@ -1,8 +1,10 @@
 let fileList = [
   { name : '正解！', url : 'music/correct1.mp3'},
   { name : '残念…', url : 'music/incorrect1.mp3'},
+  { name : '旅立ちの日に', url : 'music/tabidachi.mp3'},
 ];
 
+let playMusic = new Audio();
 let playList = '<ul>';
 for(let i=0; i < fileList.length; i++){
     playList +='<li onclick = PlayMusic("'+fileList[i].url+'")>'; //ここの意味がわからん
@@ -12,7 +14,7 @@ playList +='</ul>';
 document.getElementById("playListArea").innerHTML = playList;//ここは分かりそう。
 
 function PlayMusic(url){
-  // playMusic.pause();
+  playMusic.pause();
   playMusic = new Audio(url);
   playMusic.play();
 }
